ee4e31331b01db9222ed74574ee4411eea3efd78
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-10-06 20:03:50 +0700

    chore: Add JWT connection

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://ace-starfish-34.clerk.accounts.dev",
+      domain: "https://refined-muskox-9.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
